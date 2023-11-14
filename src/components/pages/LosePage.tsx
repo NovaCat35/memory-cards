a3db77655dcc66114f93f34e196a2fbc4acb58ed
Robert Chen <bergintonatcapital@gmail.com>
2023-11-13 20:26:44 -0500

    Edit ending screens to be video instead of gif

--- src/components/pages/LosePage.tsx
+++ src/components/pages/LosePage.tsx
@@ -1,28 +1,33 @@
 import { useContext, useState, useEffect } from "react";
 import { pageContextType, pageContext } from "../../App";
-import defeatGif from "../../assets/defeat.gif";
+// import defeatGif from "../../assets/defeat.gif";
 import "../../styles/DefeatPage.scss";
+import backgroundVideo from "../../assets/raiden-shogun-meditating.mp4";
+
 
 function LosePage() {
 	const { setLoseActive } = useContext<pageContextType>(pageContext);
-	const [gifSrc, setGifSrc] = useState(defeatGif);
+	// const [gifSrc, setGifSrc] = useState(defeatGif);
 
-	const reloadGif = () => {
-		setGifSrc("");
-		setTimeout(() => {
-			setGifSrc(defeatGif);
-		}, 0);
-	};
+	// const reloadGif = () => {
+	// 	setGifSrc("");
+	// 	setTimeout(() => {
+	// 		setGifSrc(defeatGif);
+	// 	}, 0);
+	// };
 
-	useEffect(() => {
-		reloadGif(); // Reload the GIF when the component mounts
-	}, []);
+	// useEffect(() => {
+	// 	reloadGif(); // Reload the GIF when the component mounts
+	// }, []);
 
 	return (
 		<div className="main-lose-container">
+			<video autoPlay muted loop playsInline id="backgroundVideo">
+				<source src={backgroundVideo} type="video/mp4" />
+			</video>
 			<h1>Challenge Failed</h1>
 			<p>Looks like you're not going to make it out of Tenshukaku alive today.</p>
-			<img src={gifSrc} alt="victory gif" />
+			{/* <img src={gifSrc} alt="victory gif" /> */}
 			<button onClick={() => setLoseActive(false)}>Try again?</button>
 		</div>
 	);
--- src/components/pages/WinPage.tsx
+++ src/components/pages/WinPage.tsx
@@ -1,28 +1,33 @@
 import { useContext, useState, useEffect } from "react";
 import { pageContextType, pageContext } from "../../App";
-import victoryGif from '../../assets/victory.gif'
+// import victoryGif from '../../assets/victory.gif'
 import "../../styles/WinPage.scss";
+import backgroundVideo from "../../assets/furina-running.mp4";
 
 function WinPage() {
   const { setWinActive } = useContext<pageContextType>(pageContext);
-  const [gifSrc, setGifSrc] = useState(victoryGif);
+  // const [gifSrc, setGifSrc] = useState(victoryGif);
 
-  const reloadGif = () => {
-    setGifSrc('');
-    setTimeout(() => {
-      setGifSrc(victoryGif);
-    }, 0);
-  };
+  // const reloadGif = () => {
+  //   setGifSrc('');
+  //   setTimeout(() => {
+  //     setGifSrc(victoryGif);
+  //   }, 0);
+  // };
 
-  useEffect(() => {
-    reloadGif(); // Reload the GIF when the component mounts
-  }, []);
+  // useEffect(() => {
+  //   reloadGif(); // Reload the GIF when the component mounts
+  // }, []);
 
   return (
-    <div className="main-lose-container">
-      <h1>You Made It, Well Played!</h1>
-      <img src={gifSrc} alt="victory gif" />
-      <button onClick={() => setWinActive(false)}>Go again?</button>
+    <div className="main-win-container">
+      <h1>You Did It, Bravo!</h1>
+      <p>Raise your glasses in celebration! If you don't have one, then just raise your hand in lieu.</p>
+      <video autoPlay muted loop playsInline id="backgroundVideo">
+				<source src={backgroundVideo} type="video/mp4" />
+			</video>
+      {/* <img src={gifSrc} alt="victory gif" /> */}
+      <button onClick={() => setWinActive(false)}>Onwards?</button>
     </div>
   )
 }
