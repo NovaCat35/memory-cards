1f2c2b98eed3799cb9377343bc5fd3c362516c67
Robert Chen <bergintonatcapital@gmail.com>
2023-11-16 14:03:22 -0500

    Fixing minor naming mistakes

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -1,13 +1,13 @@
 import {useContext} from "react";
 import {pageContextType, pageContext} from "../App";
-import "../styles/ScoreBoard.scss"
+import "../styles/Scoreboard.scss"
 
 function Scoreboard() {
    const {currScore, bestScore} = useContext<pageContextType>(pageContext);
 
   return (
     <div className="main-scoreboard">
-      <div className="curr-score"><h2>Score</h2><div className="score">{currScore}</div></div>
+      <div className="current-score"><h2>Score</h2><div className="score">{currScore}</div></div>
       <div className="best-score"><h2>Best Score</h2>{bestScore}</div>
     </div>
   )
